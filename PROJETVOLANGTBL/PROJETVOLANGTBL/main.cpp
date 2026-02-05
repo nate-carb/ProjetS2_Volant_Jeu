5238ba3398e8a34609d970cab6fe3368006116c4
SamDubuc <dubucsamuel@outlook.com>
2026-02-05 17:43:08 -0500

    track print

--- PROJETVOLANGTBL/PROJETVOLANGTBL/main.cpp
+++ PROJETVOLANGTBL/PROJETVOLANGTBL/main.cpp
@@ -1,12 +1,22 @@
 #include "PROJETVOLANGTBL.h"
 #include "map2d.h"
 #include <QtWidgets/QApplication>
+#include <windows.h>
+#include <iostream>
+#include <QDebug>
+#include <chrono>
 
 int main(int argc, char *argv[])
 {
-    Map2D track;
-    track.drawMapASCII();
-    
+    AllocConsole();  // Creates a console window
+    FILE* fp;
+    freopen_s(&fp, "CONOUT$", "w", stdout);  // Redirects cout to console
+
+    // Your code here
+    Map2D map;
+    map.drawMapASCII();
+
+    system("pause");  // Keeps console open to see the output
     QApplication app(argc, argv);
     PROJETVOLANGTBL window;
     window.show();
